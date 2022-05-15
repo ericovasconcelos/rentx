5d53230faedd07e9888d52bb6917a7b9918aafa6
Erico Vasconcelos <erico@ettbrasil.com.br>
2022-05-15 12:22:26 -0300

    [RM] Removed ios and android folders

--- ios/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  rentx
-//
-//  Created by Erico Vasconcelos on 03/05/22.
-//
-
-import Foundation
--- ios/wmelon.swift
+++
@@ -1,8 +0,0 @@
-//
-//  wmelon.swift
-//  rentx
-//
-//  Created by Erico Vasconcelos on 02/05/22.
-//
-
-import Foundation
